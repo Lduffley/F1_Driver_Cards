5de05b501b25a5835819867903970c32377254b7
Lduffley <53620669+Lduffley@users.noreply.github.com>
2020-02-13 10:07:37 -0600

    Updated Drivers Info for 2020

--- src/drivers.js
+++ src/drivers.js
@@ -1,41 +1,42 @@
 const drivers = [
   {
     id: 1,
-    name: "Daniel Ricciadro",
+    name: "Lewis Hamilton",
     imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/daniel-ricciardo/_jcr_content/image.img.1920.medium.jpg/1554819092086.jpg",
+      "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.1920.medium.jpg/1554818913486.jpg",
     helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/daniel-ricciardo/_jcr_content/helmet.img.png/1552565540881.png",
-    number: 3,
-    team: "Renault F1 Team",
+      "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/helmet.img.png/1551351963565.png",
+    number: 44,
+    team: "Mercedes",
     yearsOld: {
-      born: "1989-07-01",
-      age: 30
+      born: "1985-01-07",
+      age: 35
     },
-    country: "Australia",
-    podiums: 29,
-    points: 1040,
-    grandPrix: 171,
+    country: "United Kingdom",
+    podiums: 151,
+    points: 3431,
+    grandPrix: 250,
     highestPol: 1
   },
   {
     id: 2,
-    name: "Lando Norris",
+    name: "Valtteri Bottas",
     imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/lando-norris/_jcr_content/image.img.1920.medium.jpg/1567085899345.jpg",
+      "https://www.formula1.com/content/fom-website/en/drivers/valtteri-bottas/_jcr_content/image.img.1920.medium.jpg/1554818929157.jpg",
     helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/lando-norris/_jcr_content/helmet.img.png/1567520464890.png",
-    number: 4,
-    team: "McLaren",
+      "https://www.formula1.com/content/fom-website/en/drivers/valtteri-bottas/_jcr_content/helmet.img.png/1551351962160.png",
+    number: 77,
+    team: "Mercedes",
     yearsOld: {
-      born: "1999-11-13",
-      age: 20
+      born: "1989-08-28",
+      age: 30
     },
-    country: "United Kingdom",
-    podiums: "N/A",
-    points: 49,
-    grandPrix: 21,
-    highestPol: 5
+    country: "Finland",
+    podiums: 45,
+    points: 1289,
+    grandPrix: 140,
+    highestPol: 1
+    
   },
   {
     id: 3,
@@ -58,82 +59,6 @@ const drivers = [
   },
   {
     id: 4,
-    name: "Kimi Raikkonen",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/kimi-raikkonen/_jcr_content/image.img.1920.medium.jpg/1554819054944.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/kimi-raikkonen/_jcr_content/helmet.img.png/1551352334990.png",
-    number: 7,
-    team: "Alfa Romeo",
-    yearsOld: {
-      born: "1979-10-17 ",
-      age: 40
-    },
-    country: "Finland",
-    podiums: 120,
-    points: 2985,
-    grandPrix: 241,
-    highestPol: 1
-  },
-  {
-    id: 5,
-    name: "Romain Grosjean",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/romain-grosjean/_jcr_content/image.img.1920.medium.jpg/1568719868304.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/romain-grosjean/_jcr_content/helmet.img.png/1551351963006.png",
-    number: 8,
-    team: "Haas F1 Team",
-    yearsOld: {
-      born: "1986-04-17",
-      age: 33
-    },
-    country: "France",
-    podiums: 10,
-    points: 389,
-    grandPrix: 166,
-    highestPol: 2
-  },
-  {
-    id: 6,
-    name: "Pierre Gasly",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/pierre-gasly/_jcr_content/image.img.1920.medium.jpg/1566295417688.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/pierre-gasly/_jcr_content/helmet.img.png/1551351962002.png",
-    number: 10,
-    team: "Toro Rosso",
-    yearsOld: {
-      born: "1996-02-07",
-      age: 23
-    },
-    country: "France",
-    podiums: 1,
-    points: 124,
-    grandPrix: 47,
-    highestPol: 4
-  },
-  {
-    id: 7,
-    name: "Sergio Perez",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/sergio-perez/_jcr_content/image.img.1920.medium.jpg/1554818944774.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/sergio-perez/_jcr_content/helmet.img.png/1552565540424.png",
-    number: 11,
-    team: "Racing Point",
-    yearsOld: {
-      born: "1990-01-26",
-      age: 29
-    },
-    country: "Mexico",
-    podiums: 8,
-    points: 581,
-    grandPrix: 178,
-    highestPol: 4
-  },
-  {
-    id: 8,
     name: "Charles Leclerc",
     imgURL:
       "https://www.formula1.com/content/fom-website/en/drivers/charles-leclerc/_jcr_content/image.img.1920.medium.jpg/1567179277948.jpg",
@@ -152,45 +77,26 @@ const drivers = [
     highestPol: 1
   },
   {
-    id: 9,
-    name: "Lance Stroll",
+    id: 5,
+    name: "Max Verstappen",
     imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/lance-stroll/_jcr_content/image.img.1920.medium.jpg/1554818895505.jpg",
+      "https://www.formula1.com/content/fom-website/en/drivers/max-verstappen/_jcr_content/image.img.1920.medium.jpg/1554819019533.jpg",
     helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/lance-stroll/_jcr_content/helmet.img.png/1552565540999.png",
-    number: 18,
-    team: "Racing Point",
+      "https://www.formula1.com/content/fom-website/en/drivers/max-verstappen/_jcr_content/helmet.img.png/1552565541062.png",
+    number: 33,
+    team: "Red Bull Racing",
     yearsOld: {
-      born: "1998-10-29",
-      age: 21
+      born: "1997-09-30",
+      age: 22
     },
-    country: "Canada",
-    podiums: 1,
-    points: 67,
-    grandPrix: 62,
-    highestPol: 2
+    country: "Netherlands",
+    podiums: 31,
+    points: 948,
+    grandPrix: 102,
+    highestPol: 1
   },
   {
-    id: 10,
-    name: "Kevin Magnussen",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/kevin-magnussen/_jcr_content/image.img.1920.medium.jpg/1568719901149.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/kevin-magnussen/_jcr_content/helmet.img.png/1551351958111.png",
-    number: 20,
-    team: "Haas F1 Team",
-    yearsOld: {
-      born: "1992-10-05",
-      age: 2
-    },
-    country: "Denmark",
-    podiums: 1,
-    points: 157,
-    grandPrix: 103,
-    highestPol: 4
-  },
-  {
-    id: 11,
+    id: 6,
     name: "Alex Albon",
     imgURL:
       "https://www.formula1.com/content/fom-website/en/drivers/alexander-albon/_jcr_content/image.img.1920.medium.jpg/1566295320855.jpg",
@@ -207,6 +113,101 @@ const drivers = [
     points: 92,
     grandPrix: 21,
     highestPol: 5
+  },
+  {
+    id: 7,
+    name: "Carlos Sainz Jr.",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/carlos-sainz/_jcr_content/image.img.1920.medium.jpg/1567085905449.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/carlos-sainz/_jcr_content/helmet.img.png/1567520464991.png",
+    number: 55,
+    team: "McLaren",
+    yearsOld: {
+      born: "1994-09-01",
+      age: 25
+    },
+    country: "Spain",
+    podiums: 1,
+    points: 267,
+    grandPrix: 102,
+    highestPol: 5
+  },
+  {
+    id: 8,
+    name: "Lando Norris",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/lando-norris/_jcr_content/image.img.1920.medium.jpg/1567085899345.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/lando-norris/_jcr_content/helmet.img.png/1567520464890.png",
+    number: 4,
+    team: "McLaren",
+    yearsOld: {
+      born: "1999-11-13",
+      age: 20
+    },
+    country: "United Kingdom",
+    podiums: "N/A",
+    points: 49,
+    grandPrix: 21,
+    highestPol: 5
+  },
+  {
+    id: 9,
+    name: "Daniel Ricciadro",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/daniel-ricciardo/_jcr_content/image.img.1920.medium.jpg/1554819092086.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/daniel-ricciardo/_jcr_content/helmet.img.png/1552565540881.png",
+    number: 3,
+    team: "Renault F1 Team",
+    yearsOld: {
+      born: "1989-07-01",
+      age: 30
+    },
+    country: "Australia",
+    podiums: 29,
+    points: 1040,
+    grandPrix: 171,
+    highestPol: 1
+  },
+  {
+    id: 10,
+    name: "Esteban Ocon",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/esteban-ocon/_jcr_content/image.img.1920.medium.jpg/1579790911718.jpg",
+    helmet:
+      "https://www.formula1.com/etc/designs/default/0.gif",
+    number: 31,
+    team: "Renault F1 Team",
+    yearsOld: {
+      born: "1996-09-17",
+      age: 24
+    },
+    country: "France",
+    podiums: "N/A",
+    points: 136,
+    grandPrix: 50,
+    highestPol: 3
+  },
+  {
+    id: 11,
+    name: "Pierre Gasly",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/pierre-gasly/_jcr_content/image.img.1920.medium.jpg/1566295417688.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/pierre-gasly/_jcr_content/helmet.img.png/1551351962002.png",
+    number: 10,
+    team: "Toro Rosso",
+    yearsOld: {
+      born: "1996-02-07",
+      age: 23
+    },
+    country: "France",
+    podiums: 1,
+    points: 124,
+    grandPrix: 47,
+    highestPol: 4
   },
   {
     id: 12,
@@ -229,115 +230,63 @@ const drivers = [
   },
   {
     id: 13,
-    name: "Esteban Ocon",
+    name: "Sergio Perez",
     imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/esteban-ocon/_jcr_content/image.img.1920.medium.jpg/1579790911718.jpg",
+      "https://www.formula1.com/content/fom-website/en/drivers/sergio-perez/_jcr_content/image.img.1920.medium.jpg/1554818944774.jpg",
     helmet:
-      "https://www.formula1.com/etc/designs/default/0.gif",
-    number: 31,
-    team: "Renault F1 Team",
+      "https://www.formula1.com/content/fom-website/en/drivers/sergio-perez/_jcr_content/helmet.img.png/1552565540424.png",
+    number: 11,
+    team: "Racing Point",
     yearsOld: {
-      born: "1996-09-17",
-      age: 24
+      born: "1990-01-26",
+      age: 29
     },
-    country: "France",
-    podiums: "N/A",
-    points: 136,
-    grandPrix: 50,
-    highestPol: 3
+    country: "Mexico",
+    podiums: 8,
+    points: 581,
+    grandPrix: 178,
+    highestPol: 4
   },
   {
     id: 14,
-    name: "Max Verstappen",
+    name: "Lance Stroll",
     imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/max-verstappen/_jcr_content/image.img.1920.medium.jpg/1554819019533.jpg",
+      "https://www.formula1.com/content/fom-website/en/drivers/lance-stroll/_jcr_content/image.img.1920.medium.jpg/1554818895505.jpg",
     helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/max-verstappen/_jcr_content/helmet.img.png/1552565541062.png",
-    number: 33,
-    team: "Red Bull Racing",
+      "https://www.formula1.com/content/fom-website/en/drivers/lance-stroll/_jcr_content/helmet.img.png/1552565540999.png",
+    number: 18,
+    team: "Racing Point",
     yearsOld: {
-      born: "1997-09-30",
-      age: 22
+      born: "1998-10-29",
+      age: 21
     },
-    country: "Netherlands"
+    country: "Canada",
+    podiums: 1,
+    points: 67,
+    grandPrix: 62,
+    highestPol: 2
   },
   {
     id: 15,
-    name: "Lewis Hamilton",
+    name: "Kimi Raikkonen",
     imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.1920.medium.jpg/1554818913486.jpg",
+      "https://www.formula1.com/content/fom-website/en/drivers/kimi-raikkonen/_jcr_content/image.img.1920.medium.jpg/1554819054944.jpg",
     helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/helmet.img.png/1551351963565.png",
-    number: 44,
-    team: "Mercedes",
+      "https://www.formula1.com/content/fom-website/en/drivers/kimi-raikkonen/_jcr_content/helmet.img.png/1551352334990.png",
+    number: 7,
+    team: "Alfa Romeo",
     yearsOld: {
-      born: "1985-01-07",
-      age: 35
+      born: "1979-10-17 ",
+      age: 40
     },
-    country: "United Kingdom"
+    country: "Finland",
+    podiums: 120,
+    points: 2985,
+    grandPrix: 241,
+    highestPol: 1
   },
   {
     id: 16,
-    name: "Carlos Sainz Jr.",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/carlos-sainz/_jcr_content/image.img.1920.medium.jpg/1567085905449.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/carlos-sainz/_jcr_content/helmet.img.png/1567520464991.png",
-    number: 55,
-    team: "McLaren",
-    yearsOld: {
-      born: "1994-09-01",
-      age: 25
-    },
-    country: "Spain"
-  },
-  {
-    id: 17,
-    name: "George Russell",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/george-russell/_jcr_content/image.img.1920.medium.jpg/1554818804832.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/george-russell/_jcr_content/helmet.img.png/1551351959067.png",
-    number: 63,
-    team: "Williams",
-    yearsOld: {
-      born: "1998-02-15",
-      age: 21
-    },
-    country: "United Kingdom"
-  },
-  {
-    id: 18,
-    name: "Valtteri Bottas",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/valtteri-bottas/_jcr_content/image.img.1920.medium.jpg/1554818929157.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/valtteri-bottas/_jcr_content/helmet.img.png/1551351962160.png",
-    number: 77,
-    team: "Mercedes",
-    yearsOld: {
-      born: "1989-08-28",
-      age: 30
-    },
-    country: "Finland"
-  },
-  {
-    id: 19,
-    name: "Robert Kubica",
-    imgURL:
-      "https://www.formula1.com/content/fom-website/en/drivers/robert-kubica/_jcr_content/image.img.1920.medium.jpg/1554818824881.jpg",
-    helmet:
-      "https://www.formula1.com/content/fom-website/en/drivers/robert-kubica/_jcr_content/helmet.img.png/1551351964749.png",
-    number: 88,
-    team: "Williams",
-    yearsOld: {
-      born: "1984-12-07",
-      age: 35
-    },
-    country: "Poland"
-  },
-  {
-    id: 20,
     name: "Antonio Giovinazzi",
     imgURL:
       "https://www.formula1.com/content/fom-website/en/drivers/antonio-giovinazzi/_jcr_content/image.img.1920.medium.jpg/1554818879413.jpg",
@@ -349,8 +298,89 @@ const drivers = [
       born: "1993-12-14",
       age: 26
     },
-    country: "Italy"
-  }
+    country: "Italy",
+    podiums: "N/A",
+    points: 14,
+    grandPrix: 23,
+    highestPol: 7
+  },
+  {
+    id: 17,
+    name: "Romain Grosjean",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/romain-grosjean/_jcr_content/image.img.1920.medium.jpg/1568719868304.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/romain-grosjean/_jcr_content/helmet.img.png/1551351963006.png",
+    number: 8,
+    team: "Haas F1 Team",
+    yearsOld: {
+      born: "1986-04-17",
+      age: 33
+    },
+    country: "France",
+    podiums: 10,
+    points: 389,
+    grandPrix: 166,
+    highestPol: 2
+  },
+  {
+    id: 18,
+    name: "Kevin Magnussen",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/kevin-magnussen/_jcr_content/image.img.1920.medium.jpg/1568719901149.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/kevin-magnussen/_jcr_content/helmet.img.png/1551351958111.png",
+    number: 20,
+    team: "Haas F1 Team",
+    yearsOld: {
+      born: "1992-10-05",
+      age: 2
+    },
+    country: "Denmark",
+    podiums: 1,
+    points: 157,
+    grandPrix: 103,
+    highestPol: 4
+  },
+  {
+    id: 19,
+    name: "George Russell",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/george-russell/_jcr_content/image.img.1920.medium.jpg/1554818804832.jpg",
+    helmet:
+      "https://www.formula1.com/content/fom-website/en/drivers/george-russell/_jcr_content/helmet.img.png/1551351959067.png",
+    number: 63,
+    team: "Williams",
+    yearsOld: {
+      born: "1998-02-15",
+      age: 21
+    },
+    country: "United Kingdom",
+    podiums: "N/A",
+    points: 0,
+    grandPrix: 21,
+    highestPol: 14
+  },
+  {
+    id: 20,
+    name: "Nicholas Latifi",
+    imgURL:
+      "https://www.formula1.com/content/fom-website/en/drivers/nicholas-latifi/_jcr_content/image.img.1536.medium.jpg/1578580980422.jpg",
+    helmet:
+      "https://www.formula1.com/etc/designs/default/0.gif",
+    number: 6,
+    team: "Williams",
+    yearsOld: {
+      born: "1995-29-06",
+      age: 25
+    },
+    country: "Canada",
+    podiums: "N/A",
+    points: 0,
+    grandPrix: "N/A",
+    highestPol: "N/A"
+  },
+
 ];
 
 export default drivers;
